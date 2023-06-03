6f359ddf3663b6953d6745144b137a91c9a4eacc
starik061 <starik061@gmail.com>
2023-06-03 10:27:45 +0300

    refactor all card code

--- src/catalog-pg/catalog/catalog.js
+++ src/catalog-pg/catalog/catalog.js
@@ -1,34 +1,13 @@
 import img from '../../images/no-poster-img.png';
-import renderPhotoCard from '../../common/card/card.js';
+import { renderPhotoCard } from '../../common/card/card.js';
 const catalogElement = document.querySelector('.catalog-section');
 
 const catalogURL =
   'https://api.themoviedb.org/3/trending/all/week?page=1&per_page=20';
-const filmGenres = [
-  { id: 28, name: 'Action' },
-  { id: 12, name: 'Adventure' },
-  { id: 16, name: 'Animation' },
-  { id: 35, name: 'Comedy' },
-  { id: 80, name: 'Crime' },
-  { id: 99, name: 'Documentary' },
-  { id: 18, name: 'Drama' },
-  { id: 10751, name: 'Family' },
-  { id: 14, name: 'Fantasy' },
-  { id: 36, name: 'History' },
-  { id: 27, name: 'Horror' },
-  { id: 10402, name: 'Music' },
-  { id: 9648, name: 'Mystery' },
-  { id: 10749, name: 'Romance' },
-  { id: 878, name: 'Science Fiction' },
-  { id: 10770, name: 'TV Movie' },
-  { id: 53, name: 'Thriller' },
-  { id: 10752, name: 'War' },
-  { id: 37, name: 'Western' },
-];
 
 createCardsCatalog(catalogURL);
 
-function createCardsCatalog(URL) {
+export default function createCardsCatalog(URL) {
   const options = {
     method: 'GET',
     headers: {
@@ -41,63 +20,9 @@ function createCardsCatalog(URL) {
   fetch(URL, options)
     .then(response => response.json())
     .then(response => {
-      response.results.forEach(
-        ({ title, poster_path, genre_ids, release_date, vote_average }) => {
-          let genresInCard = getGenresToFilmCard(genre_ids, filmGenres);
-          let filmYear = getFilmYear(release_date);
-          let filmRaiting = getRating(vote_average);
-
-          let filmInfoObject = {
-            title,
-            fullPath: poster_path
-              ? 'https://image.tmdb.org/t/p/w500' + poster_path
-              : img,
-            genresInCard,
-            filmYear,
-            filmRaiting,
-          };
-
-          renderPhotoCard(filmInfoObject, catalogElement);
-        }
-      );
+      response.results.forEach(filmInfoObject => {
+        renderPhotoCard(filmInfoObject, catalogElement);
+      });
     })
     .catch(err => console.error(err));
-}
-
-function getGenresToFilmCard(genresArray, allgenres) {
-  let genres = '-';
-  let firstGenre = '';
-  let secondGenre = '';
-  const MAX_GENRES_STRING_LENGTH = 14;
-  for (let i = 0; i < genresArray.length; i += 1) {
-    if (allgenres.find(el => el.id === genresArray[i])) {
-      firstGenre = allgenres.find(el => el.id === genresArray[i]).name;
-    } else {
-      continue;
-    }
-    if (firstGenre && i != genresArray.length - 1) {
-      secondGenre = allgenres.find(el => el.id === genresArray[i + 1]).name;
-
-      break;
-    }
-  }
-  if (firstGenre.length + secondGenre.length > MAX_GENRES_STRING_LENGTH) {
-    genres = firstGenre;
-    return genres;
-  }
-  genres = firstGenre + ', ' + secondGenre;
-  return genres;
-}
-
-function getFilmYear(date) {
-  if (!date) {
-    return '-';
-  }
-  date = date.split('-');
-  return date[0];
-}
-
-function getRating(ratingData) {
-  ratingData = ratingData.toFixed(1);
-  return `${ratingData * 10}%`;
 }

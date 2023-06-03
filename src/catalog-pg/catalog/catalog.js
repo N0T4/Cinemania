99b4fdfe6fa6318b6bfe3bd19ee4a20d9621953d
starik061 <starik061@gmail.com>
2023-06-03 11:46:12 +0300

    refctoring card Code

--- src/catalog-pg/catalog/catalog.js
+++ src/catalog-pg/catalog/catalog.js
@@ -1,28 +1,7 @@
-import { renderPhotoCard } from '../../common/card/card.js';
+import { createCardsCatalog } from '../../common/card/card.js';
 const catalogElement = document.querySelector('.catalog-section');
 
 const catalogURL =
   'https://api.themoviedb.org/3/trending/all/week?page=1&per_page=20';
 
-createCardsCatalog(catalogURL);
-
-export default function createCardsCatalog(URL) {
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDYyYWMyZWNiZjNmYjk1YWM5Mzc1ZWNmMDY1Y2M5NiIsInN1YiI6IjY0Nzg1NDRhMDc2Y2U4MDBjNTBhZDZmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dj9JHo6BS7b-nDH8R0SXWEYbjX-5KPxmVhRCe0GPRME',
-    },
-  };
-
-  fetch(URL, options)
-    .then(response => response.json())
-    .then(response => {
-      console.log(response.results);
-      response.results.forEach(filmInfoObject => {
-        renderPhotoCard(filmInfoObject, catalogElement);
-      });
-    })
-    .catch(err => console.error(err));
-}
+createCardsCatalog(catalogURL, catalogElement);
--- src/common/card/card.js
+++ src/common/card/card.js
@@ -61,6 +61,26 @@ class CardInfo {
   }
 }
 
+function createCardsCatalog(URL, parentElement) {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDYyYWMyZWNiZjNmYjk1YWM5Mzc1ZWNmMDY1Y2M5NiIsInN1YiI6IjY0Nzg1NDRhMDc2Y2U4MDBjNTBhZDZmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dj9JHo6BS7b-nDH8R0SXWEYbjX-5KPxmVhRCe0GPRME',
+    },
+  };
+
+  fetch(URL, options)
+    .then(response => response.json())
+    .then(response => {
+      response.results.forEach(filmInfoObject => {
+        renderPhotoCard(filmInfoObject, parentElement);
+      });
+    })
+    .catch(err => console.error(err));
+}
+
 const card = new CardInfo();
 
-export { card, renderPhotoCard };
+export { card, renderPhotoCard, createCardsCatalog };

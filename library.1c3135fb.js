830e84e6a417ad08419b2e002c302808b1faec29
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-06-04 11:51:26 +0000

    Deploying to gh-pages from @ N0T4/Cinemania@d71a0437703662c0dc588e8aa604b6703e3b7da6 🚀

---
+++ library.1c3135fb.js
@@ -0,0 +1,2 @@
+!function(){var e=!0;document.querySelector('.theme-switcher [type="checkbox"]').addEventListener("change",(function(){console.log(1),document.body.classList.toggle("light-theme"),e=!e}))}();
+//# sourceMappingURL=library.1c3135fb.js.map
---
+++ library.1e015bc1.js
@@ -0,0 +1,2 @@
+let e=!0;document.querySelector('.theme-switcher [type="checkbox"]').addEventListener("change",(function(){console.log(1),document.body.classList.toggle("light-theme"),e=!e}));
+//# sourceMappingURL=library.1e015bc1.js.map

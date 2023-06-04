6520cb8adbd2d923040db5f9834d36b96cd57247
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-06-04 20:50:18 +0000

    Deploying to gh-pages from @ N0T4/Cinemania@3af7c82a2cdfb43ae51b2eeb42c53a1ab988ed5a 🚀

--- library.1e015bc1.js
+++
@@ -1,2 +0,0 @@
-let e=!0;document.querySelector('.theme-switcher [type="checkbox"]').addEventListener("change",(function(){console.log(1),document.body.classList.toggle("light-theme"),e=!e}));
-//# sourceMappingURL=library.1e015bc1.js.map

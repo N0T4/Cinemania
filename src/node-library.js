d71a0437703662c0dc588e8aa604b6703e3b7da6
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-06-04 12:50:58 +0100

    regrouped JS files

---
+++ src/node-library.js
@@ -0,0 +1,4 @@
+import './library-save/saved-films/saved.js';
+import './library-save/load-more/load-more.js';
+import './library-save/genres/genres.js';
+import './library-save/add-to-library/add.js';

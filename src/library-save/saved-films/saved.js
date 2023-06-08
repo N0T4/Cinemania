b4ad9ce815be94776db1bd4c38c1258533b00898
starik061 <starik061@gmail.com>
2023-06-08 19:55:29 +0300

    little fix for null

--- src/library-save/saved-films/saved.js
+++ src/library-save/saved-films/saved.js
@@ -3,11 +3,11 @@ const myLibraryElement = document.querySelector('.my-library-section');
 
 let storedFilms = JSON.parse(localStorage.getItem('my library'));
 
-console.log(typeof storedFilms);
-
 // function createMyLibraryCatalog(savedFilms, parentElement) {
 //   savedFilms.map(film => {});
 // }
-storedFilms.forEach(film => {
-  renderPhotoCard(film, myLibraryElement);
-});
+if (storedFilms) {
+  storedFilms.forEach(film => {
+    renderPhotoCard(film, myLibraryElement);
+  });
+}

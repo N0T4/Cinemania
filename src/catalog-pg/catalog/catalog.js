94ca1d8a02c8ef090a4b5fb7ac88e4336d579466
N0T4 <118122479+N0T4@users.noreply.github.com>
2023-06-08 08:57:25 +0100

    fix-catalog

--- src/catalog-pg/catalog/catalog.js
+++ src/catalog-pg/catalog/catalog.js
@@ -1,6 +1,6 @@
-// import { createCardsCatalog } from '../../common/card/card.js';
-// import createPagination from '../pagination/pagination.js';
+ import { createCardsCatalog } from '../../common/card/card.js';
+ import createPagination from '../pagination/pagination.js';
 
-// const catalogElement = document.querySelector('.catalog-section');
-// const catalogURL = `https://api.themoviedb.org/3/trending/all/week?page=1`;
-// createCardsCatalog(catalogURL, catalogElement);
+ const catalogElement = document.querySelector('.catalog-section');
+ const catalogURL = `https://api.themoviedb.org/3/trending/all/week?page=1`;
+ createCardsCatalog(catalogURL, catalogElement);

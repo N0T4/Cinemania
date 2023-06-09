329d80c1fd3368d4e80c3013dd6e7b03deedd6a3
KalchenkoNatalya <janatka2222@gmail.com>
2023-06-09 15:49:33 +0300

    кількість карток на сторінці

--- src/catalog-pg/search/api.js
+++ src/catalog-pg/search/api.js
@@ -4,8 +4,7 @@ export class MovieApi {
   #API_KEY = '57ca3cbf384960d64107a3c5d70155b8';
   #TRENDING_URL = 'https://api.themoviedb.org/3/trending/all/week';
   #SEARCH_URL = 'https://api.themoviedb.org/3/search/movie';
-  #YEAR_URL =  'https://api.themoviedb.org/3/discover/movie';
- 
+  #YEAR_URL = 'https://api.themoviedb.org/3/discover/movie';
 
   #query = '';
   #selectYear = '';
@@ -22,27 +21,36 @@ export class MovieApi {
   }
 
   async getTrendingMovieData(page) {
-    const response = await axios.get(
+    const { data } = await axios.get(
       `${this.#TRENDING_URL}?page=${page}&api_key=${this.#API_KEY}`
     );
-    console.log(response);
-    return response.data;
+    data.results = data.results.slice(0, 18);
+    console.log(data);
+    return data;
   }
-
   async getMoviesByQueryData(page) {
-    const response = await axios.get(
+    const { data } = await axios.get(
       `${this.#SEARCH_URL}?query=${this.#query}&primary_release_year=${
         this.#selectYear
       }&region=${this.#selectCountry}&api_key=${this.#API_KEY}&page=${page}`
     );
-    console.log(response);
-    return response.data;
+    data.results = data.results.slice(0, 18);
+    console.log(data);
+
+    return data;
   }
+
+
   async getFilmYearData(page) {
-    const response = await axios.get(
-      `${this.#YEAR_URL}?include_adult=false&include_video=false&primary_release_year=${this.#selectYear}&sort_by=popularity.desc&api_key=${this.#API_KEY}&page=${page}`
+    const { data } = await axios.get(
+      `${
+        this.#YEAR_URL
+      }?include_adult=false&include_video=false&primary_release_year=${
+        this.#selectYear
+      }&sort_by=popularity.desc&api_key=${this.#API_KEY}&page=${page}`
     );
-    console.log(response);
-    return response.data;
+    data.results = data.results.slice(0, 18);
+    console.log(data);
+    return data;
   }
 }
